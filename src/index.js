70c640518b4f1ea335e4b631814b756d4e4626e6
edwyncoder <edwyn.t@flyerssoft.com>
2023-09-14 22:27:15 +0530

    configured smpt server usering nodemailer

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const EmailService = require('./services/email-service');
 
 const setupAndStartServer = async () => {
     const app = express();
@@ -10,7 +11,7 @@ const setupAndStartServer = async () => {
 
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-        
+        EmailService.sendBasicEmail("test@example.com","edwynbtrocks@gmail.com","this is subject to","thsi is the bosy oft he email");
     });
 }
 
