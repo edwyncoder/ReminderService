0276515a9c2c6b8611c758d88f39999c3249536e
edwyncoder <edwyn.t@flyerssoft.com>
2023-09-23 12:42:13 +0530

    added notification cron job

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 const EmailService = require('./services/email-service');
+const {startJob} = require("./utils/job");
+const TicketController = require('./controllers/ticket-controller');
+
 
 const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
 
+    app.post('/api/v1/tickets', TicketController.create);
+
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
         // EmailService.sendBasicEmail("test@example.com","edwynbtrocks@gmail.com","this is subject to","thsi is the bosy oft he email");
+        startJob()
     });
 }
 
--- src/services/email-service.js
+++ src/services/email-service.js
@@ -1,5 +1,5 @@
 const sender = require('../config/emailConfig');
-
+const TicketRepository = require('../repository/ticket-repository');
 
 const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
     try {
@@ -14,9 +14,41 @@ const sendBasicEmail = async (mailFrom, mailTo, mailSubject, mailBody) => {
         console.log(error);
     }
 }
+const repo = new TicketRepository();
+
+const createNotification = async (data) => {
+    try {
+        console.log(data);
+        const response = await repo.create(data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const fetchPendingEmails = async (timestamp) => {
+    try {
+        const response = await repo.get({status: "PENDING"});
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updateTicket = async (ticketId, data) => {
+    try {
+        const response = await repo.update(ticketId, data);
+        return response;
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 module.exports = {
     sendBasicEmail,
+    createNotification,
+    fetchPendingEmails,
+    updateTicket
 }
 
 
